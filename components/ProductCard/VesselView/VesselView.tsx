ec0f2f79c9b7c1d0021cb95da6a34668f64886a0
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2024-07-14 12:23:58 +0100

    Fix product card v 1.0

--- components/ProductCard/VesselView/VesselView.tsx
+++ components/ProductCard/VesselView/VesselView.tsx
@@ -61,39 +61,14 @@ export const VesselView: React.FC<Props> = ({ ves, images }) => {
                 alt="full screen button"
               />
             </span>
-            <Slider images={images} />
+            <Slider images={images}/>
           </div>
           <div className={styles.body__bottom}>
             <h1 className={typo.typo_h4}>About</h1>
             <div className={styles.body__about}>
               <div className={styles.body__about_text}>
-                <p>
-                  Discover the epitome of maritime luxury with our stunning
-                  yacht, &quot;Ocean Serenity.&quot; This sleek and elegant
-                  vessel combines cutting-edge design with unparalleled comfort,
-                  offering a truly indulgent experience on the open seas. With a
-                  length of 120 feet and a beam of 28 feet, Ocean Serenity
-                  provides ample space for relaxation, entertainment, and
-                  exploration.
-                  <br />
-                  Ocean Serenity boasts a modern, sophisticated design with a
-                  stylish exterior and luxurious interior finishes. From the
-                  moment you step on board, you are enveloped in an atmosphere
-                  of opulence and refinement.
-                  <br />
-                  The interior spaces are meticulously designed, featuring
-                  high-quality materials and exquisite craftsmanship. The yacht
-                  accommodates up to 12 guests in 6 beautifully appointed
-                  cabins, each with ensuite bathrooms. The master suite is a
-                  sanctuary of comfort, offering panoramic views of the ocean
-                  and a private lounge area.
-                  <br />
-                  Ocean Serenity offers a wide range of entertainment options,
-                  including a state-of-the-art home theater system, a
-                  well-stocked library, and a spacious lounge area. The outdoor
-                  deck features a Jacuzzi, perfect for unwinding under the sun
-                  or stars. Additionally, there is a fully equipped bar and
-                  grill for delightful al fresco dining experiences.
+                <p className={styles.body__about_featch}>
+                  <span>{ves.yacht_description}</span>
                 </p>
               </div>
               <div className={styles.body__about_featchures}>
@@ -118,16 +93,20 @@ export const VesselView: React.FC<Props> = ({ ves, images }) => {
                   <span>{ves.yacht_berth}</span>
                 </p>
                 <p className={styles.body__about_featch}>
+                  <span>Shower:</span>
+                  <span>{ves.yacht_shower}</span>
+                </p>
+                <p className={styles.body__about_featch}>
+                  <span>Heads:</span>
+                  <span>{ves.yacht_heads}</span>
+                </p>
+                <p className={styles.body__about_featch}>
                   <span>Kell Type:</span>
                   <span>{ves.yacht_keel_type}</span>
                 </p>
                 <p className={styles.body__about_featch}>
                   <span>Fuel Type:</span>
                   <span>{ves.yacht_fuel_type}</span>
-                </p>
-                <p className={styles.body__about_featch}>
-                  <span>Engine Quantity:</span>
-                  <span>{ves.yacht_engine}</span>
                 </p>
               </div>
             </div>
@@ -137,14 +116,14 @@ export const VesselView: React.FC<Props> = ({ ves, images }) => {
           <div className={styles.form_top}>
             <span>VAT Included</span>{' '}
             <span className={styles.form_modal}>
-              <QuestionMarkSvg />
+              <QuestionMarkSvg/>
             </span>
           </div>
           <div className={styles.form}>
             <h3 className={`${typo.typo_h4} ${styles.form_header}`}>
               Contact us
             </h3>
-            <ContactForm productCard={true} />
+            <ContactForm productCard={true}/>
           </div>
         </div>
       </div>
